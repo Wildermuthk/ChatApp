90505f40b3a32cf9dde5c90c4926ae2fc22cf239
Wildermuthk <Wildermuthk@cwu.edu>
2024-11-30 20:38:34 -0800

    Added sign out capabilities and a token to tell if the user is signed in.

--- app/src/main/java/com/example/chatapp/utilities/Constants.java
+++ app/src/main/java/com/example/chatapp/utilities/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String KEY_USER_ID = "userid";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public static final String KEY_IMAGE = "image";
+    public static final String KEY_FCM_TOKEN = "fcmToken";
     public static final String KEY_PREFERENCE_NAME = "chatAppReference";
 
 
